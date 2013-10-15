8832484ae685c4e12bdb28c3cfe8793d8a6a7351
Jairo Gatjens <jgatjens@gmail.com>
2013-10-15 17:53:35 -0600

    initial architecture for the books restful api

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-books-backend
-=============
+Book's Backend 
+=========
 
-Restful api
+Book es una aplicación HTML5 que nos permetira:
+
+  - obtener bases solidas de Javascript
+  - trabajar con flujos reales
+  - usar la metodologia agile para la planeación del proyecto
+  - optimización del proceso de desarrollo, calidad y producion 
+
+Book va hacer una pequeña aplicacion que listara todos los libros de tecnologia que esten en papel. Una ves un usuario este logeado podra saber quien esta leyendo algun libro de interes o podra reservar un libro.  
+
+Version
+----
+
+1.0
+
+Tech
+-----------
+
+Estas son las tecnologias a usar:
+
+* [Nodejs]  - javascript en el backend.
+* [mongodb] - base de datos nosql
+* [Gruntjs] - organizador de tareas 
+* [Bower]   - manejador de dependencias en el frontend 
+* [Express] - node.js framework  [@tjholowaychuk]
+* [Sass] - pre compilador de css
+* [jQuery] - duh 
+
+
+
+Dependencias
+--------------
+Haber instaldo mongodb y por supuesto tenerlo corriendo.
+
+Instalación del backend
+--------------
+
+```sh
+git clone [git-repo-url] backend
+cd backend
+npm install
+```
+```sh
+node app
+```
+
+
+
+
+
+  
+    
---
+++ app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'), 
+    routes = require('./routes'),
+    // sass  = require('node-sass'),
+    // fs = require('fs');
+    http = require('http');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 4000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+
+  // app.use(express.compress());
+  
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+
+  // app.use(express.session( secret: 'inuyasha'));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+app.get('/', routes.backbone);
+app.get('/app-jquery', routes.jquery);
+
+app.get('/api/books', routes.books.all);
+app.get('/api/books/:id', routes.books.one);
+// app.post('/api/books', routes.books.create);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
