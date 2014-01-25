f33ac88ac03b1ada194f07056ae000b8475edb8a
Jairo Gatjens <jgatjens@gmail.com>
2014-01-25 13:51:45 -0600

    add - mongodb instance

--- conf/database.js
+++ conf/database.js
@@ -1,5 +1,10 @@
 
 var dbUrl = process.env.NODE_ENV === "test" ? "library_test": "library";
+
+if (process.env.NODE_ENV === "production" ) {
+	dbUrl = "mongodb://heroku_app21581708:vcgoumt6r89kmvmcvsbhnck3tq@ds027769.mongolab.com:27769/heroku_app21581708";
+}	
+
 var collections = ["books","users", "sessions"];
 var db = require("mongojs").connect(dbUrl, collections);
 
