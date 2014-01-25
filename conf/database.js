2709e6bed06bf7d66424e9652bbee5eb8bbb6b0e
Jairo Gatjens <jgatjens@gmail.com>
2014-01-25 14:11:00 -0600

    update - config db to add heroku mongolab env var

--- conf/database.js
+++ conf/database.js
@@ -1,11 +1,22 @@
 
-var dbUrl = process.env.NODE_ENV === "test" ? "library_test": "library";
+// var dbUrl = process.env.NODE_ENV === "test" ? "library_test": "library";
 
-if (process.env.NODE_ENV === "production" ) {
-	dbUrl = "mongodb://heroku_app21581708:vcgoumt6r89kmvmcvsbhnck3tq@ds027769.mongolab.com:27769/heroku_app21581708";
-}	
+// console.log(process.env);
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'library';
 
 var collections = ["books","users", "sessions"];
-var db = require("mongojs").connect(dbUrl, collections);
+var db = require("mongojs").connect(mongoUri, collections);
+
+// console.log(db);
+
+// var mongo = require('mongodb');
+
+// var db = mongo.Db.connect(mongoUri, function (err, db) {
+//   db.collection('mydocs', function(er, collection) {
+//     collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+//     });
+//   });
+// });
 
 module.exports = db;
